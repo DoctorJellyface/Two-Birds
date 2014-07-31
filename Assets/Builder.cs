1f7120f25ad212864c9caff366c0ae30488629cd
DoctorJellyface <DoctorJellyface@users.noreply.github.com>
2014-07-31 11:59:20 +0200

    Added licenses to source code.

--- Assets/Builder.cs
+++ Assets/Builder.cs
@@ -1,3 +1,21 @@
+﻿// 
+// Two Birds - a little game inspired by (but not quite like) Flappy Bird
+// Copyright (C) 2014 Juraj Fiala <doctorjellyface@riseup.net>
+// 
+// This file is part of Two Birds.
+// 
+//     Two Birds is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     Two Birds is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with Two Birds.  If not, see <http://www.gnu.org/licenses/>.
 
 using UnityEngine;
 using System.Collections;
